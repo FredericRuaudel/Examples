4f2b4130e78be13ffe890e2ca9b8696cfdcead1e
Peter Zignego <pzignego@gmail.com>
2017-07-14 16:44:37 -0400

    Update Package.swift

--- robot-or-not-bot/Package.swift
+++ robot-or-not-bot/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "robot-or-not-bot",
     dependencies: [
-        .Package(url: "https://github.com/SlackKit/SlackKit", "4.0.0")
+        .Package(url: "https://github.com/SlackKit/SlackKit", majorVersion: 4)
     ]
 )
